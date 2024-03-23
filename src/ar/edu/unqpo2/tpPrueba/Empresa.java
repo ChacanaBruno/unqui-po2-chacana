8222da4ddd6cca8fad58d821b5309fecb4b2978d
Bruno <Bruno@192.168.0.15>
2024-03-23 14:09:12 -0600

    Se creo la Clase Empresa la cual tiene una lista de empleados y de haberes

--- src/ar/edu/unqpo2/tpPrueba/Empresa.java
+++ src/ar/edu/unqpo2/tpPrueba/Empresa.java
@@ -1,41 +1,44 @@
 package ar.edu.unqpo2.tpPrueba;
-//Clase para representar un empleado
-public class Empleado {
- private String nombre;
- private String cuit;
 
- // Constructor
- public Empleado(String nombre, String cuit) {
-     this.nombre = nombre;
-     this.cuit = cuit;
- }
-
- // Getters y setters
- public String getNombre() {
-     return nombre;
- }
-
- public void setNombre(String nombre) {
-     this.nombre = nombre;
- }
-
- public String getCuit() {
-     return cuit;
- }
-
- public void setCuit(String cuit) {
-     this.cuit = cuit;
- }
-}
+import java.util.ArrayList;
+import java.util.List;
 
 public class Empresa {
-	private String nombre ;
+	private String nombre;
 	private String cuit;
-	private List<Empleado> empleados; // falta la clase empleado
-}
-
-public Empresa(String nombre, String cuit) {
+	private List<Empleado> empleados;
+	private List<ReciboDeHaberes> reciboDeHaberes;
+	public Empresa(String nombre, String cuit, List<Empleado> empleados, List<ReciboDeHaberes> reciboDeHaberes) {
+		super();
 		this.nombre = nombre;
 		this.cuit = cuit;
-		
-} 
+		this.empleados = empleados;
+		this.reciboDeHaberes = reciboDeHaberes;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCuit() {
+		return cuit;
+	}
+	public void setCuit(String cuit) {
+		this.cuit = cuit;
+	}
+	public List<Empleado> getEmpleados() {
+		return empleados;
+	}
+	public void setEmpleados(List<Empleado> empleados) {
+		this.empleados = empleados;
+	}
+	public List<ReciboDeHaberes> getReciboDeHaberes() {
+		return reciboDeHaberes;
+	}
+	public void setReciboDeHaberes(List<ReciboDeHaberes> reciboDeHaberes) {
+		this.reciboDeHaberes = reciboDeHaberes;
+	}
+	
+	
+}
