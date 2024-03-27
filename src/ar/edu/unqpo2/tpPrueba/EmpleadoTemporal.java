fb0e6ec60ed840cbece6fd1f297bb3ff96fa393d
Bruno <Bruno@192.168.0.17>
2024-03-27 17:55:24 -0600

    empleado temporal ahora sabe calcular su sueldo bruto

--- src/ar/edu/unqpo2/tpPrueba/EmpleadoTemporal.java
+++ src/ar/edu/unqpo2/tpPrueba/EmpleadoTemporal.java
@@ -5,12 +5,14 @@ import java.time.LocalDate;
 public class EmpleadoTemporal extends Empleado {
 	private LocalDate fechaFinDeAsigcion;
 	private int cantidadDeHorasExtras;
+	//constructor
 	public EmpleadoTemporal(String nombre, String cuit, String estadoCivil, LocalDate fechaNacimiento,
 			Double sueldoBasico, LocalDate fechaFinDeAsigcion, int cantidadDeHorasExtras) {
 		super(nombre, cuit, estadoCivil, fechaNacimiento, sueldoBasico);
 		this.fechaFinDeAsigcion = fechaFinDeAsigcion;
 		this.cantidadDeHorasExtras = cantidadDeHorasExtras;
 	}
+	//getters and setters
 	public LocalDate getFechaFinDeAsigcion() {
 		return fechaFinDeAsigcion;
 	}
@@ -22,6 +24,25 @@ public class EmpleadoTemporal extends Empleado {
 	}
 	public void setCantidadDeHorasExtras(int cantidadDeHorasExtras) {
 		this.cantidadDeHorasExtras = cantidadDeHorasExtras;
+	}
+	
+
+	@Override
+	public double calcularSueldoBruto() {
+		/* El sueldo bruto del empleado de planta temporaria se compone de:
+			Sueldo Básico
+			Horas Extras: $40 por cada hora extra*/
+		
+		double sueldoBasico = getSueldoBasico() ;
+		double montoPorHorasExtras = getCantidadDeHorasExtras() * 40 ;
+		double sueldoBruto = sueldoBasico + montoPorHorasExtras;
+		
+		return sueldoBruto;
+	}
+	@Override
+	public double calcularRetenciones() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	
